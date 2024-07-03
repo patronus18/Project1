41d79f141ee8b19000e066e898f21b2bc0a8da2d
patronus18 <anagha.udupa@collins.com>
2024-07-03 11:39:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ app = Flask(__name__)
 app.secret_key = 'supersecretkey'
 
 # Adjust the path to the directory containing your HTML files
-html_dir = os.path.join(os.path.dirname(__file__), 'C:\Users\E40085777\flammability_project\templates')
+html_dir = r'C:\Users\E40085777\flammability_project\templates'
+
 
 csv_file_path = 'sample data.csv'
 
